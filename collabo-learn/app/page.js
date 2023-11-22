eab1678b591669544dc7b9d9dfdb4d6da6e35dcb
Vincemanio <john.manio@edu.sait.ca>
2023-11-21 23:52:27 -0700

    organize!

--- collabo-learn/app/page.js
+++ collabo-learn/app/page.js
@@ -1,113 +1,89 @@
-import Image from 'next/image'
+import React from 'react';
 
-export default function Home() {
+// Navbar component
+const Navbar = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <nav className="flex justify-between items-center py-4 px-6 shadow-md bg-white text-black font-anonymous">
+      <div className="text-xl font-semibold text-black">Collabo-Learn</div>
+      <div className="space-x-4">
+        <a href="#home" className="hover:underline text=black">Home</a>
+        <a href="#collaborate" className="hover:underline text=black">Collaborate</a>
+        <a href="#find-tutor" className="hover:underline text=black">Find a tutor</a>
+        <a href="#sample-projects" className="hover:underline text=black">Sample Projects</a>
       </div>
+    </nav>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+// Hero component for the main banner with text and image side by side
+const Hero = () => {
+  return (
+    <div className="flex justify-between items-center bg-blue-200 px-6" style={{ height: '263px' }}>
+      <img 
+        src="https://techpando.com/wp-content/uploads/2022/09/Uses-of-Laptop-in-Our-Daily-Life.jpg"
+        alt="Collaborative workspace"
+        className="mr-20 w-auto" // Added right margin of 1rem (mr-4)
+        style={{ width: '552px', height: '263px' }} // Inline styles for exact sizing
+      />
+      <div className="flex-1">
+        <h1 className="text-3xl font-bold">Collaborate and Learn.</h1>
+        <p className="mt-4 text-lg">A place to share knowledge and better understand the world.</p>
+      </div>
+    </div>
+  );
+};
+
+// Section component for Web Development and UX/UI sections
+const Section = ({ title, description }) => {
+  return (
+    <div className="flex items-center p-4 border-b">
+      <div className="flex-1">
+        <h2 className="text-lg font-bold">{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="flex-none">
+        {/* Placeholder for icons */}
+      </div>
+    </div>
+  );
+};
+
+// Footer component
+const Footer = () => {
+  return (
+    <footer className="flex justify-between items-center p-4 bg-gray-200 text-black">
+      <div className="flex space-x-4">
+        {/* Placeholder for social icons */}
+      </div>
+      <div className="space-x-4">
+        <a href="#general" className="hover:underline text-black">General</a>
+        <a href="#careers" className="hover:underline text-black">Careers</a>
+      </div>
+      <div className="text-sm text-black">
+        Copyright © 2023 Collabo–Learn Inc. All Rights Reserved
+      </div>
+    </footer>
+  );
+};
+// Main App component that composes the page
+const App = () => {
+  return (
+    <div className="App bg-white">
+      <Navbar />
+      <Hero />
+      <div className="my-8">
+        <Section
+          title="Web Development"
+          description="Web development is a fascinating field that involves creating websites or web applications for the internet."
+        />
+        <Section
+          title="UX/UI"
+          description="User Experience and User Interface"
         />
       </div>
+      <Footer />
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
